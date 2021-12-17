0687e6d13ec60d0d4e4af599ab16595f3652a6ba
ahmed <ahmedgrati1999@gmail.com>
2021-12-17 16:01:14 +0100

    add TP3

---
+++ src/test/java/com/ahmedgrati/qatp3/QATP3/Qatp3ApplicationTests.java
@@ -0,0 +1,21 @@
+package com.ahmedgrati.qatp3.QATP3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+class Qatp3ApplicationTests {
+
+
+
+
+
+}
